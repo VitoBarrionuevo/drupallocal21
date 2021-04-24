3f71d4524839c55699a96ea2eeada0317d4965d0
Seba <sstechs@hotmail.com>
2021-04-24 20:03:36 -0300

    dolor de cabeza

--- web/modules/custom/custom_module_seba/src/Form/ProfileForm.php
+++ web/modules/custom/custom_module_seba/src/Form/ProfileForm.php
@@ -20,11 +20,41 @@ class ProfileForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  
+   public function buildForm(array $form, FormStateInterface $form_state) {
+    
+    $form['first_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('First Name'),
+      '#default_value' => '',
+      '#description' => $this->t('Enter your First Name.'),
+      '#weight' => -3,
+      '#required' => TRUE,
+    ];
+
+    $form['last_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Last Name'),
+      '#default_value' => '',
+      '#description' => $this->t('Enter your Last Name.'),
+      '#weight' => -2,
+      '#required' => TRUE,
+    ];
+  
+    $form['dni'] = [
+      '#type' => 'number',
+      '#title' => $this->t('D.N.I.'),
+      '#default_value' => '',
+      '#description' => $this->t('Enter your DNI number.'),
+      '#weight' => -1,
+      '#required' => TRUE,
+    ];
+  
+    
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
-    ];
+      ];
 
     return $form;
   }
