98e9353dc6310748556969b883712c916ffd52dc
jeremias <jnahuel46@gmail.com>
2021-04-24 15:28:26 -0300

    sabado

---
+++ web/modules/custom/custom_module_jere/src/Form/custom_module_entityDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\custom_module_jere\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Custom_module_entity entities.
+ *
+ * @ingroup custom_module_jere
+ */
+class custom_module_entityDeleteForm extends ContentEntityDeleteForm {
+
+
+}
