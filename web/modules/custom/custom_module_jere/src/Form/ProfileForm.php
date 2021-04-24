8a74008687e98a0a122bc062aca95d397e45578d
jeremias <jnahuel46@gmail.com>
2021-04-24 20:03:41 -0300

    module

--- web/modules/custom/custom_module_jere/src/Form/ProfileForm.php
+++ web/modules/custom/custom_module_jere/src/Form/ProfileForm.php
@@ -8,19 +8,60 @@ use Drupal\Core\Form\FormStateInterface;
 /**
  * Class ProfileForm.
  */
-class ProfileForm extends FormBase {
+class ProfileForm extends FormBase
+{
 
   /**
    * {@inheritdoc}
    */
-  public function getFormId() {
+  public function getFormId()
+  {
     return 'profile_form';
   }
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+    $form['name'] = array(
+      '#type' => 'textfield',
+      '#title' => $this
+        ->t('Name'),
+      '#default_value' => '',
+      '#size' => 20,
+      '#attributes' => array(
+        'placeholder' => t('Please insert Name'),
+      ),
+      '#maxlength' => 20,
+    
+      '#required' => TRUE,
+    );
+
+    $form['lastname'] = array(
+      '#type' => 'textfield',
+      '#title' => $this
+        ->t('Last Name'),
+      '#default_value' => '',
+      '#size' => 20,
+      '#attributes' => array(
+        'placeholder' => t('Please insert Last Name'),
+      ),
+      '#maxlength' => 20,
+      '#required' => TRUE,
+    );
+
+    $form['dni'] = array(
+      '#type' => 'number',
+      '#title' => $this
+        ->t('DNI'),
+      '#attributes' => array(
+        'placeholder' => t('Please insert DNI'),
+      ),
+      '#required' => TRUE,
+      '#maxlength' => 10,
+    );
+
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
@@ -32,7 +73,8 @@ class ProfileForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
     foreach ($form_state->getValues() as $key => $value) {
       // @TODO: Validate fields.
     }
@@ -42,11 +84,11 @@ class ProfileForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
     // Display result.
     foreach ($form_state->getValues() as $key => $value) {
-      \Drupal::messenger()->addMessage($key . ': ' . ($key === 'text_format'?$value['value']:$value));
+      \Drupal::messenger()->addMessage($key . ': ' . ($key === 'text_format' ? $value['value'] : $value));
     }
   }
-
 }
