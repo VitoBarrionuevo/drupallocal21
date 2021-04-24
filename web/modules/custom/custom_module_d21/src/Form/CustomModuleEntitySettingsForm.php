9fb2cf25662a2676c6cf4ab90d04a1f05522f185
Vito <victor.barrionuevo@darwoft.com>
2021-04-24 15:31:22 -0300

    entity and form

---
+++ web/modules/custom/custom_module_d21/src/Form/CustomModuleEntitySettingsForm.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\custom_module_d21\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class CustomModuleEntitySettingsForm.
+ *
+ * @ingroup custom_module_d21
+ */
+class CustomModuleEntitySettingsForm extends FormBase {
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'custommoduleentity_settings';
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Empty implementation of the abstract submit class.
+  }
+
+  /**
+   * Defines the settings form for Custom module entity entities.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   Form definition array.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['custommoduleentity_settings']['#markup'] = 'Settings form for Custom module entity entities. Manage field settings here.';
+    return $form;
+  }
+
+}
