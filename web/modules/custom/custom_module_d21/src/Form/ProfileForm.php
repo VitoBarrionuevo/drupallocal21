83054e95b790a4159ad7f4168d4c220fae1cb45c
jeremias <jnahuel46@gmail.com>
2021-05-08 17:43:15 -0300

    Revert "Merge branch 'master' into jere21"
    This reverts commit 774faa3ed05114badcf84dbb9d4854d8827bbf07, reversing
    changes made to a489c1facb591946d0aab62d89791e305c7c1c0e.

--- web/modules/custom/custom_module_d21/src/Form/ProfileForm.php
+++ web/modules/custom/custom_module_d21/src/Form/ProfileForm.php
@@ -4,7 +4,6 @@ namespace Drupal\custom_module_d21\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Url;
 
 /**
  * Class ProfileForm.
@@ -23,12 +22,29 @@ class ProfileForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
+    //checkbox
+    //radios
+    //datetime
+    //email
+    //file
+    //image
+    //integer
+    //link
+    //list_string dropdown select
+    //password
+    //string
+    //text_long
+
+
+
+
     $form['dni'] = [
       '#type' => 'number',
       '#title' => $this->t('D.N.I.'),
       '#default_value' => '',
       '#description' => $this->t('Enter your DNI number.'),
       '#weight' => -8,
+      '#required' => true,
       '#maxlength' => 8,
     ];
 
@@ -38,6 +54,7 @@ class ProfileForm extends FormBase {
       '#description' => $this->t('Enter your first name.'),
       '#description' => $this->t(''),
       '#weight' => -10,
+      '#required' => true,
     ];
 
     $form['last_name'] = [
@@ -46,88 +63,8 @@ class ProfileForm extends FormBase {
       '#description' => $this->t('Enter your last name.'),
       '#description' => $this->t(''),
       '#weight' => -9,
+      '#required' => true,
     ];
-
-    $form['gender'] = array(
-      '#type' => 'radios',
-      '#title' => $this->t('Gender'),
-      '#default_value' => 1,
-      '#options' => array(
-        0 => $this->t('Male'),
-        1 => $this->t('Female'),
-      ),
-    );
-
-    $form['term_cond'] = array(
-      '#type' => 'checkbox',
-      '#title' => $this->t('Accept terms and conditions'),
-    );
-
-    $form['field_date'] = array(
-      '#type' => 'date',
-      '#title' => 'Enter Your Date of Birth',
-      '#format' => 'm/d/Y',
-      '#description' => t('i.e. 09/06/2016'),
-      '#default_value' => '',
-      '#date_date_format' => 'm/d/Y',
-    );
-
-    $form['email'] = array(
-      '#type' => 'email',
-      '#title' => $this->t('Email'),
-    );
-
-    $form['my_file'] = array(
-      '#type' => 'managed_file',
-      '#name' => 'my_file',
-      '#title' => t('File *'),
-      '#size' => 20,
-      '#description' => t('PDF format only'),
-      '#upload_location' => 'public://my_files/',
-    );
-
-    $form['profile_image'] = [
-      '#type' => 'managed_file',
-      '#title' => t('Profile Picture'),
-      '#upload_validators' => array(
-        'file_validate_extensions' => array('gif png jpg jpeg'),
-        'file_validate_size' => array(25600000),
-      ),
-      '#theme' => 'image_widget',
-      '#preview_image_style' => 'medium',
-      '#upload_location' => 'public://profile-pictures',
-    ];
-
-    $form['examples_link'] = [
-      '#title' => $this->t('Serch Link'),
-      '#type' => 'url',
-      '#url' => Url::fromRoute('custom_module_d21.profile_form'),
-    ];
-
-    $form['example_select'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Select element'),
-      '#options' => [
-        '1' => $this->t('One'),
-        '2' => [
-          '2.1' => $this->t('Two point one'),
-          '2.2' => $this->t('Two point two'),
-        ],
-        '3' => $this->t('Three'),
-      ],
-    ];
-
-    $form['pass'] = array(
-      '#type' => 'password',
-      '#title' => $this->t('Password'),
-      '#size' => 25,
-    );
-
-    $form['text'] = array(
-      '#type' => 'textarea',
-      '#title' => $this->t('Text'),
-    );
-
     $form['#meses'] = 'ENERO';
 
     $form['submit'] = [
